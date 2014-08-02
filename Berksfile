a0452cabdeb277df107ee5b6aa6aef1ef03fdfc2
maeve <maeve.revels@getg5.com>
2014-08-02 11:43:33 -0700

    Initial unit tests for default recipe

--- Berksfile
+++ Berksfile
@@ -1,3 +1,7 @@
 source 'https://supermarket.getchef.com'
 
 metadata
+
+cookbook 'apt', '2.3.10'
+cookbook 'build-essential', '1.4.4'
+cookbook 'rbenv', '1.6.5'
---
+++ spec/default_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'g5_rbenv::default' do
+  let(:chef_run) do
+    ChefSpec::Runner.new.converge(described_recipe)
+  end
+
+  it 'includes the rbenv default recipe' do
+    expect(chef_run).to include_recipe('rbenv::default')
+  end
+
+  it 'includes the ruby_build recipe' do
+    expect(chef_run).to include_recipe('rbenv::ruby_build')
+  end
+
+  it 'installs the default global ruby'
+  it 'installs bundler in the default ruby'
+  it 'sets permissions on the rbenv dir'
+  it 'adds vagrant to the rbenv group'
+end
