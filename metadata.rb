3a204a49c6f595d8bdf8b49ea1095764892cad86
maeve <maeve.revels@getg5.com>
2014-08-02 10:02:53 -0700

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,9 @@
-name "vagrant_rbenv"
-depends "rbenv"
+name 'g5_rbenv'
+description 'Wrapper cookbook for rbenv'
+version '0.0.1'
+maintainer 'G5'
+license 'MIT'
+
+replaces 'vagrant_rbenv'
+
+depends 'rbenv'
